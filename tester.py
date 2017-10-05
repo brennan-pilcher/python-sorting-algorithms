0f686b5d6620147308a6e161fbd7056f2f375dea
Brennan Pilcher <brennan.pilcher@gmail.com>
2017-10-04 23:48:58 -0400

    Implemented selection sort and added test case to tester.

--- tester.py
+++ tester.py
@@ -4,7 +4,11 @@
 
 # Imports
 from insertion import insertion_sort
+from selection import selection_sort
 
 short_list = [5, 1, 3, 2, 4, 7, 6, 8, 9, 0]
 
 print( *insertion_sort(short_list) )
+
+
+print( *selection_sort(short_list) )
